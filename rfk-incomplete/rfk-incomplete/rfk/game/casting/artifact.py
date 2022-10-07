0e802dc12619aa758c9ed576917b537397b1caf4
AGB004 <alecbyington@gmail.com>
2022-10-06 20:21:12 -0600

    update artifact class

--- rfk-incomplete/rfk-incomplete/rfk/game/casting/artifact.py
+++ rfk-incomplete/rfk-incomplete/rfk/game/casting/artifact.py
@@ -5,3 +5,10 @@ class Artifact(Actor):
 
     def __init__(self):
         super().__init__()
+        self._message = ""
+
+    def set_message(self, message):
+        self._message = message
+
+    def get_message(self):
+        return self._message
